0a86410a6de3cd7cc296cb5c9a4c4318af174e84
mayusB <54024278+mayusB@users.noreply.github.com>
2024-09-11 01:22:23 +0100

    Initial commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_cmsYzMMYb',
+        userPoolClientId: '5nj0kd45h3vcu37udp7onvbckv',
+        region: 'us-east-1'
+    },
+    api: {
+        invokeUrl: 'https://zn6sfcpp07.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
